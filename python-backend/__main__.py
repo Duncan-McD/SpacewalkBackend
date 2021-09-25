e6decf90ad69350c2bf8af2518fabbf17786fd21
Duncan-McD <dam342@cornell.edu>
2021-09-25 04:40:49 -0400

    Updated backend endpoints

--- python-backend/__main__.py
+++ python-backend/__main__.py
@@ -1,14 +1,28 @@
+import sys
 import pymongo
 import json
+from argparse import ArgumentParser
 from .requester import app
 
 def main(args):
+  parser = ArgumentParser(description='''Private API for Spacewalk''', prog="spacewalk.py")
+  parser.add_argument('-d','--debug', action='store_true', 
+      help='turns on flask debug setting',
+      default=False)
+  args = parser.parse_args(args)
 
-  f = open('credentials.json')
+
+  f = open('./python-backend/credentials.json')
 
   credentials = json.load(f)
 
-  client = pymongo.MongoClient("mongodb+srv://" + credentials.username + ":" + credentials.password + "@bigredhacks2021.cojtr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-  db = client.test
+  client = pymongo.MongoClient("mongodb+srv://" + credentials["username"] + ":" + credentials["password"] + "@bigredhacks2021.cojtr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+  db = client.SpaceWalk
 
-  app.run(debug=True, host='0.0.0.0');
+  app.config["db"] = db
+  app.config["users"] = db.Users
+  app.config["bodies"] = db.Bodies
+  app.run(debug=args.debug, host='0.0.0.0')
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
--- python-backend/requester.py
+++ python-backend/requester.py
@@ -2,20 +2,69 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/users", methods["GET"])
-def search_users():
-  return [];
+@app.route("/users", methods=["GET"])
+def users():
+  cursor = app.config["users"].find({})
+  return {"users": [doc["ID"] for doc in cursor]}
+   
 
-@app.route("/user", methods["GET"])
-def search_users():
+@app.route("/user", methods=["GET"])
+def user():
   userid = request.args.get('id')
-  return {};
+  docs = list(app.config["users"].find({"ID":userid}))
+  if (len(docs)== 1): 
+    del (docs[0]['_id'])
+    return docs[0]
+  return {}
 
-@app.route("/bodies", methods["GET"])
-def search_users():
-  return [];
+@app.route("/bodies", methods=["GET"])
+def bodies():
+  cursor = app.config["bodies"].find({})
+  return {"bodies": [doc["name"] for doc in cursor]}
 
-@app.route("/body", methods["GET"])
-def search_users():
+@app.route("/body", methods=["GET"])
+def body():
   name = request.args.get('name')
-  return {};
+  docs = list(app.config["bodies"].find({"name":name}))
+  if (len(docs)== 1): 
+    del (docs[0]['_id'])
+    return docs[0]
+  return {}
+
+
+@app.route("/updateDistance", methods=["POST"])
+def updateDistance():
+  body = request.get_json()
+  userid = body['id']
+  distance = body['distance']
+  test = app.config["users"].find_one_and_update({"ID": userid}, 
+                                {"$set": {"walkedDistance": distance}})
+  return {"success": bool(test)}
+
+@app.route("/signUp", methods=["POST"])
+def signUp():
+  body = request.get_json()
+  email = body['email']
+  name = body['name']
+  password = body['password']
+  username = body['userame']
+  doc = app.config["users"].find_one({"email":email})
+  doc2 = app.config["users"].find_one({"ID":username})
+  new_user = {
+    "ID": username,
+    "name": name,
+    "email": email,
+    "password": password,
+    "walkedDistance": 0,
+    "level": 0
+  }
+  if(not len(doc) and not len(doc2)): 
+    test = app.config["users"].insert_one(new_user)
+    return {"success": bool(test)}
+  else:
+    if(len(doc) and len(doc2)):reason ="Email and username already in use! :("
+    elif(len(doc)):reason ="Email already in use! :("
+    elif(len(doc2)):reason="Username already in use! :("
+    
+    return {"success": False, "reason": reason}
+  
